e284e11803b66535605fac92b28204f3d624fc9c
adhex08 <59111521+adhex08@users.noreply.github.com>
2019-12-23 21:00:36 +0700

    Update botLogin.js

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "TOKEN_KEY",
-	youtubeAPI: "YOUTUBE_API_KEY",
-	owner_id: "OWNER_ID"
+	token: "NjU4NjYwNDc1NDk1Nzc2Mjc2.XgC_ng.4lgYdQKSn_bNZS82X95Emrh0P7c",
+	youtubeAPI: "AIzaSyC0o-DhwAMCKJ-8z16ahX8F7K3GQP08H2g",
+	owner_id: "658660475495776276"
 }
